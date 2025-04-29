884018eac49d1e04cd85e665990de5f5c3534a1a
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-04-29 18:57:39 +0200

    style(Ray): improve code formatting and comments for clarity

--- src/core/Ray.cpp
+++ src/core/Ray.cpp
@@ -6,75 +6,64 @@
 */
 
 #include "Ray.hpp"
-#include "Vector3D.hpp"
 #include "Transform.hpp"
+#include "Vector3D.hpp"
 
 namespace RayTracer {
 
-    Ray::Ray() : _origin(0, 0, 0), _direction(0, 0, 1)
-    {
-        // Default constructor initializes origin at (0,0,0) and direction towards positive Z
-    }
+Ray::Ray() : _origin(0, 0, 0), _direction(0, 0, 1) {
+  // Default constructor initializes origin at (0,0,0) and direction towards positive Z
+}
 
-    Ray::Ray(const Vector3D &origin, const Vector3D &direction)
-        : _origin(origin), _direction(direction.normalized())
-    {
-        // Constructor with origin and direction normalizes the direction vector
-    }
+Ray::Ray(const Vector3D& origin, const Vector3D& direction)
+    : _origin(origin), _direction(direction.normalized()) {
+  // Constructor with origin and direction normalizes the direction vector
+}
 
-    Ray::Ray(const Ray &other)
-        : _origin(other._origin), _direction(other._direction)
-    {
-        // Copy constructor
-    }
+Ray::Ray(const Ray& other)
+    : _origin(other._origin), _direction(other._direction) {
+  // Copy constructor
+}
 
-    Ray &Ray::operator=(const Ray &other)
-    {
-        if (this != &other) {
-            _origin = other._origin;
-            _direction = other._direction;
-        }
-        return *this;
-    }
+Ray& Ray::operator=(const Ray& other) {
+  if (this != &other) {
+    _origin = other._origin;
+    _direction = other._direction;
+  }
+  return *this;
+}
 
-    Ray::~Ray()
-    {
-        // Destructor
-    }
+Ray::~Ray() {
+  // Destructor
+}
 
-    Vector3D Ray::getOrigin() const
-    {
-        return _origin;
-    }
+Vector3D Ray::getOrigin() const {
+  return _origin;
+}
 
-    Vector3D Ray::getDirection() const
-    {
-        return _direction;
-    }
+Vector3D Ray::getDirection() const {
+  return _direction;
+}
 
-    void Ray::setOrigin(const Vector3D &origin)
-    {
-        _origin = origin;
-    }
+void Ray::setOrigin(const Vector3D& origin) {
+  _origin = origin;
+}
 
-    void Ray::setDirection(const Vector3D &direction)
-    {
-        _direction = direction.normalized();
-    }
+void Ray::setDirection(const Vector3D& direction) {
+  _direction = direction.normalized();
+}
 
-    Vector3D Ray::pointAt(double t) const
-    {
-        // Calculate a point along the ray at distance t
-        return _origin + (_direction * t);
-    }
+Vector3D Ray::pointAt(double t) const {
+  // Calculate a point along the ray at distance t
+  return _origin + (_direction * t);
+}
 
-    Ray Ray::transform(const Transform &transform) const
-    {
-        // Create a new ray with transformed origin and direction
-        Vector3D newOrigin = transform.applyToPoint(_origin);
-        Vector3D newDirection = transform.applyToVector(_direction);
+Ray Ray::transform(const Transform& transform) const {
+  // Create a new ray with transformed origin and direction
+  Vector3D newOrigin = transform.applyToPoint(_origin);
+  Vector3D newDirection = transform.applyToVector(_direction);
 
-        return Ray(newOrigin, newDirection);
-    }
+  return Ray(newOrigin, newDirection);
+}
 
-} // namespace RayTracer
+}  // namespace RayTracer
--- src/core/Ray.hpp
+++ src/core/Ray.hpp
@@ -18,81 +18,81 @@ class Transform;
  * @brief Represents a ray with an origin point and a direction vector
  */
 class Ray {
-public:
-    /**
+ public:
+  /**
      * @brief Default constructor
      * Creates a ray at origin (0,0,0) pointing towards positive Z direction
      */
-    Ray();
+  Ray();
 
-    /**
+  /**
      * @brief Constructor with origin and direction
      * @param origin The origin point of the ray
      * @param direction The direction vector of the ray (will be normalized)
      */
-    Ray(const Vector3D &origin, const Vector3D &direction);
+  Ray(const Vector3D& origin, const Vector3D& direction);
 
-    /**
+  /**
      * @brief Copy constructor
      * @param other The ray to copy
      */
-    Ray(const Ray &other);
+  Ray(const Ray& other);
 
-    /**
+  /**
      * @brief Assignment operator
      * @param other The ray to assign from
      * @return Reference to this ray after assignment
      */
-    Ray &operator=(const Ray &other);
+  Ray& operator=(const Ray& other);
 
-    /**
+  /**
      * @brief Destructor
      */
-    ~Ray();
+  ~Ray();
 
-    /**
+  /**
      * @brief Get the origin of the ray
      * @return The origin point
      */
-    Vector3D getOrigin() const;
+  Vector3D getOrigin() const;
 
-    /**
+  /**
      * @brief Get the direction of the ray
      * @return The direction vector (normalized)
      */
-    Vector3D getDirection() const;
+  Vector3D getDirection() const;
 
-    /**
+  /**
      * @brief Set the origin of the ray
      * @param origin The new origin point
      */
-    void setOrigin(const Vector3D &origin);
+  void setOrigin(const Vector3D& origin);
 
-    /**
+  /**
      * @brief Set the direction of the ray
      * @param direction The new direction vector (will be normalized)
      */
-    void setDirection(const Vector3D &direction);
+  void setDirection(const Vector3D& direction);
 
-    /**
+  /**
      * @brief Calculate a point along the ray at a specific distance
      * @param t The distance along the ray
      * @return The point at distance t from the origin along the ray direction
      */
-    Vector3D pointAt(double t) const;
+  Vector3D pointAt(double t) const;
 
-    /**
+  /**
      * @brief Transform the ray using a transformation matrix
      * @param transform The transformation to apply
      * @return A new ray with the transformation applied
      */
-    Ray transform(const Transform &transform) const;
+  Ray transform(const Transform& transform) const;
 
-private:
-    Vector3D _origin;    ///< The origin point of the ray
-    Vector3D _direction; ///< The normalized direction vector of the ray
+ private:
+  Vector3D _origin;     ///< The origin point of the ray
+  Vector3D _direction;  ///< The normalized direction vector of the ray
 };
 
-} // namespace RayTracer
+}  // namespace RayTracer
 
 #endif /* !RAY_HPP_ */
