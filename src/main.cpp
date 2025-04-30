cd0f584ab21fde2308230bfe499c8e5dda533602
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-04-30 10:08:40 +0200

    refactor: update Makefile for clang format and reorganize main.cpp includes; add unit tests for Vector3D class

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,29 @@
 ** main
 */
 
-#include "scene/Scene.hpp"
-#include "display/PPMDisplay.hpp"
-#include "scene/SceneBuilder.hpp"
 #include <iostream>
+#include "display/PPMDisplay.hpp"
+#include "scene/Scene.hpp"
+#include "scene/SceneBuilder.hpp"
 
-void usage()
-{
-    std::cout << "USAGE" << std::endl;
-    std::cout << "\t./raytracer <scene_file>" << std::endl;
-    std::cout << "DESCRIPTION" << std::endl;
-    std::cout << "\t<scene_file>\tPath to the scene file to be rendered." << std::endl;
+void usage() {
+  std::cout << "USAGE" << std::endl;
+  std::cout << "\t./raytracer <scene_file>" << std::endl;
+  std::cout << "DESCRIPTION" << std::endl;
+  std::cout << "\t<scene_file>\tPath to the scene file to be rendered."
+            << std::endl;
 }
 
-int main(int argc, char** argv)
-{
-    if (argc != 2) {
-        usage();
-        return 84;
-    }
-    try {
-        // launch core
-    } catch(const std::exception& e) {
-        std::cerr << e.what() << std::endl; return 84;
-    }
-    return 0;
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    usage();
+    return 84;
+  }
+  try {
+    // launch core
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return 84;
+  }
+  return 0;
 }
