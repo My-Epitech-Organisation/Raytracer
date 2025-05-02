37cd6a4f5847fca1db4d9b93804264d652c8d23f
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-05-02 17:46:18 -0500

    feat(Resolution): add Resolution class and corresponding unit tests

---
+++ src/scene/Resolution.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** Resolution
+*/
+
+#ifndef RESOLUTION_HPP_
+#define RESOLUTION_HPP_
+
+class Resolution {
+public:
+    Resolution(int width, int height) : _width(width), _height(height) {}
+
+    int getWidth() const { return _width; }
+    int getHeight() const { return _height; }
+
+    bool operator==(const Resolution& other) const {
+        return _width == other._width && _height == other._height;
+    }
+
+private:
+    int _width;
+    int _height;
+};
+
+#endif  /* !RESOLUTION_HPP_ */
