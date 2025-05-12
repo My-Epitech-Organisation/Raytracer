00474fc99b2a3c26aeeb48ba08e5acf5eeeb997b
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-05-12 15:19:14 +0200

    refactor(tests): update Light tests to remove AmbientLight and DirectionalLight references

--- tests/test_Light.cpp
+++ tests/test_Light.cpp
@@ -7,42 +7,27 @@
 
 #include <gtest/gtest.h>
 #include <sstream>
-#include "../src/scene/lights/AmbientLight.hpp"
-#include "../src/scene/lights/DirectionalLight.hpp"
 #include "../src/scene/lights/Light.hpp"
 
 using namespace RayTracer;
 
-// Test AmbientLight instead of PointLight which doesn't exist yet
-TEST(AmbientLightTest, Initialization) {
-  Vector3D color(0.5, 0.5, 0.5);
-  AmbientLight light(color);
+TEST(LightTest, BasicInitialization) {
+  std::vector<Vector3D> points = {Vector3D(1, 2, 3)};
+  std::vector<Vector3D> dirs = {Vector3D(0, -1, 0)};
+  Light light(0.5f, 0.8f, points, dirs);
 
-  // Test the properties we can actually access
-  EXPECT_TRUE(light.getColor().isEqual(Vector3D(0.5, 0.5, 0.5)));
+  EXPECT_FLOAT_EQ(light.getAmbient(), 0.5f);
+  EXPECT_FLOAT_EQ(light.getDiffuse(), 0.8f);
+  EXPECT_EQ(light.getPointLights().size(), 1);
+  EXPECT_EQ(light.getDirectionalLights().size(), 1);
 }
 
-TEST(DirectionalLightTest, Initialization) {
-  Vector3D direction(1.0, 0.0, 0.0);
-  Vector3D color(0.8, 0.6, 0.4);
-  DirectionalLight light(direction, color);
+TEST(LightTest, ToStringOutput) {
+  Light light(0.2f, 0.3f, {Vector3D(1, 2, 3)}, {Vector3D(4, 5, 6)});
+  std::string str = light.toString();
 
-  // Test the accessible properties without using getType()
-  EXPECT_TRUE(light.getDirection().isEqual(Vector3D(1.0, 0.0, 0.0)));
-  EXPECT_TRUE(light.getColor().isEqual(Vector3D(0.8, 0.6, 0.4)));
-}
-
-TEST(AmbientLightTest, ToString) {
-  AmbientLight light(Vector3D(0.1, 0.2, 0.3));
-
-  // Test properties directly without converting to string
-  EXPECT_TRUE(light.getColor().isEqual(Vector3D(0.1, 0.2, 0.3)));
-}
-
-TEST(DirectionalLightTest, ToString) {
-  DirectionalLight light(Vector3D(0, 1, 0), Vector3D(1, 1, 1));
-
-  // Test properties directly without converting to string
-  EXPECT_TRUE(light.getDirection().isEqual(Vector3D(0, 1, 0)));
-  EXPECT_TRUE(light.getColor().isEqual(Vector3D(1, 1, 1)));
+  EXPECT_NE(str.find("Ambient: 0.2"), std::string::npos);
+  EXPECT_NE(str.find("Diffuse: 0.3"), std::string::npos);
+  EXPECT_NE(str.find("Vector3D(1, 2, 3)"), std::string::npos);
+  EXPECT_NE(str.find("Vector3D(4, 5, 6)"), std::string::npos);
 }
--- tests/test_LightFactory.cpp
+++ tests/test_LightFactory.cpp
@@ -6,55 +6,35 @@
 */
 
 #include <gtest/gtest.h>
-#include "../src/scene/lights/AmbientLight.hpp"
-#include "../src/scene/lights/DirectionalLight.hpp"
 #include "../src/scene/lights/LightFactory.hpp"
+#include "../src/scene/parser/SceneParser.hpp"
 
 using namespace RayTracer;
 
-TEST(LightFactoryTests, CreateAmbientLight) {
+TEST(SceneParserTest, ParseLightConfiguration) {
   libconfig::Config cfg;
-  cfg.readString(R"(
-    light: {
-      type = "ambient";
-      color = [0.2, 0.3, 0.4];
-      intensity = 0.6;
+  const char* configText = R"(
+    lights: {
+      ambient = 0.4;
+      diffuse = 0.6;
+      point = (
+        { x = 400; y = 100; z = 500; }
+      );
+      directional = (
+        { x = 0; y = -1; z = 0; }
+      );
     };
-  )");
+  )";
 
-  const auto& setting = cfg.lookup("light");
-  std::unique_ptr<Light> light = LightFactory::createLight(setting);
+  try {
+    cfg.readString(configText);
+    const auto& lightsSetting = cfg.lookup("lights");
 
-  ASSERT_NE(dynamic_cast<AmbientLight*>(light.get()), nullptr);
-}
+    SceneParser parser;
+    parser.parseLights(lightsSetting);
 
-TEST(LightFactoryTests, CreateDirectionalLight) {
-  libconfig::Config cfg;
-  cfg.readString(R"(
-    light: {
-      type = "directional";
-      color = [1.0, 0.9, 0.8];
-      intensity = 1.2;
-      direction = [0, -1, 0];
-    };
-  )");
-
-  const auto& setting = cfg.lookup("light");
-  std::unique_ptr<Light> light = LightFactory::createLight(setting);
-
-  ASSERT_NE(dynamic_cast<DirectionalLight*>(light.get()), nullptr);
-}
-
-TEST(LightFactoryTests, UnknownTypeThrows) {
-  libconfig::Config cfg;
-  cfg.readString(R"(
-    light: {
-      type = "mystic";
-      color = [1, 1, 1];
-      intensity = 1;
-    };
-  )");
-
-  const auto& setting = cfg.lookup("light");
-  ASSERT_THROW(LightFactory::createLight(setting), std::runtime_error);
+    SUCCEED();
+  } catch (const std::exception& e) {
+    FAIL() << "Parsing lights failed: " << e.what();
+  }
 }
