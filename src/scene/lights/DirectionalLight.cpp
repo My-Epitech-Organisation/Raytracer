e820c77b07821662a618b2ec0d699dd197dae493
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-05-15 21:06:17 +0200

    feat: implement DirectionalLight methods for light behavior and properties

--- src/scene/lights/DirectionalLight.cpp
+++ src/scene/lights/DirectionalLight.cpp
@@ -6,11 +6,53 @@
 */
 
 #include "DirectionalLight.hpp"
+#include <limits>
+#include <cmath>
 
 namespace RayTracer {
 
 DirectionalLight::DirectionalLight(const Vector3D& direction)
-    : _direction(direction) {}
+    : _direction(direction.normalized()) {}
+
+Vector3D DirectionalLight::getDirectionFrom(const Vector3D& point) const {
+  // Directional light has the same direction regardless of point
+  return -_direction; // Negative because we want direction FROM point TO light
+}
+
+double DirectionalLight::getDistanceFrom(const Vector3D& point) const {
+  // Directional lights are infinitely far away
+  return std::numeric_limits<double>::infinity();
+}
+
+double DirectionalLight::getIntensityAt(const Vector3D& point) const {
+  // Directional lights have constant intensity
+  return 1.0;
+}
+
+Color DirectionalLight::getColor() const {
+  return _color;
+}
+
+bool DirectionalLight::castsShadows() const {
+  return true;
+}
+
+Ray DirectionalLight::getShadowRay(const Vector3D& point) const {
+  Vector3D direction = -_direction; // Direction from point to light
+  // Small epsilon to avoid self-shadowing
+  return Ray(point + direction * 0.001, direction);
+}
+
+std::shared_ptr<ILight> DirectionalLight::clone() const {
+  return std::make_shared<DirectionalLight>(_direction);
+}
+
+Vector3D DirectionalLight::getPosition() const {
+  // Directional lights don't have a position, return a position infinitely far in the direction
+  return Vector3D(std::numeric_limits<double>::infinity() * _direction.getX(),
+                 std::numeric_limits<double>::infinity() * _direction.getY(),
+                 std::numeric_limits<double>::infinity() * _direction.getZ());
+}
 
 const Vector3D& DirectionalLight::getDirection() const {
   return _direction;
